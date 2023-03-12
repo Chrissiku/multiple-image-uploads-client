5327f5e2e3d924ce149050dd50779300bf294b9b
Chris Siku <chrissiku5@gmail.com>
2023-03-12 16:55:28 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Upload multiple images To Rails 7 API - Active_storage : Part 1
+    </div>
+  );
+}
+
+export default App;
